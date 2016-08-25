22dd5d737114b9109415e7284792dc7da9d885dd
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 12:56:13 -0700

    com

--- server.js
+++ server.js
@@ -7,6 +7,3 @@ app.listen(port);
 console.log('Server now listening on port ' + port);
 
 //comment
-
-// #!/bin/sh
-// git --work-tree=/var/www/128.199.33.157:4568 --git-dir=/var/repo/site.git checkout -f 
